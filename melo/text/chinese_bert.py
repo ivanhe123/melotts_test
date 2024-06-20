2624538b03cfe6dd73c55e23a6aaba3b1490cc8c
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2024-06-20 09:42:00 +0800

    Update chinese_bert.py

--- melo/text/chinese_bert.py
+++ melo/text/chinese_bert.py
@@ -10,7 +10,7 @@ from transformers import AutoTokenizer, AutoModelForMaskedLM
 tokenizers = {}
 models = {}
 
-def get_bert_feature(text, word2ph, device=None, model_id='./hfl/chinese-roberta-wwm-ext-large'):
+def get_bert_feature(text, word2ph, device=None, model_id='hfl/chinese-roberta-wwm-ext-large'):
     if model_id not in models:
         models[model_id] = AutoModelForMaskedLM.from_pretrained(
             model_id
